008cedb4afba7776140f672f5fc6d682b4a2629b
Kashirof <92455082+Kashirof@users.noreply.github.com>
2021-10-15 18:57:11 +0300

    Add files via upload
    I think this s mistake but... i do it  better... tomorrow....

---
+++ app/controllers/tasker_controller.rb
@@ -0,0 +1,5 @@
+class TaskerController < ApplicationController
+    def index
+    
+    end
+end
---
+++ test/controllers/tasker_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class TaskerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
